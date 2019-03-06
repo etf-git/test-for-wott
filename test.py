bcd9bb281e7e56e960b227bea9611c6ab2941695
etf-git <48278387+etf-git@users.noreply.github.com>
2019-03-06 23:33:16 +0600

    Update test.py

--- test.py
+++ test.py
@@ -10,9 +10,19 @@ class unittests(unittest.TestCase):
   def test_is_bootstrapping(self):
   #test def is_bootstrapping() for return True
       self.assertTrue(is_bootstrapping())
+	  
   def test_can_read_cert(self):
-  #test def is_bootstrapping() check for correct function, no exit
-    self.assertTrue(self.assertTrue()!=1)
+  #test def can_read_cert() check for correct function, no exit
+	  capturedOutput = StringIO.StringIO()
+      sys.stdout = capturedOutput
+      can_read_cert()
+      sys.stdout = sys.__stdout__
+	  printtest=capturedOutput.getvalue()
+	  if(printtest!='Permission denied when trying to read the certificate file.' and printtest!='Permission denied when trying to read the key file.'):resulttest=True
+	  else:resulttest=False
+	  
+      self.assertTrue(resulttest)
+	  
   def test_get_primary_ip(self):
   #test def get_primary_ip() checking for an explicit return value and for matching the result against the pattern of the ip-address
       self.assertTrue(get_primary_ip()!=None)
@@ -23,6 +33,7 @@ class unittests(unittest.TestCase):
 	  isipresult=True
       except ValueError: isipresult=False
 	  self.assertTrue(isipresult)
+	  
   def test_get_certificate_expiration_date(self):
   #test def get_certificate_expiration_date() whether the returned result is a date
       try:
@@ -31,15 +42,19 @@ class unittests(unittest.TestCase):
       except ValueError:
       correctDate = False
       self.assertTrue(correctDate)
+	  
   def test_time_for_certificate_renewal(self):
   #test def time_for_certificate_renewal() for return True
       self.assertTrue(time_for_certificate_renewal())
+	  
   def test_generate_device_id(self):
   #test def generate_device_id() checking for an explicit return value
       self.assertTrue(generate_device_id()!=None)
+	  
   def test_get_device_id(self):
   #test def get_device_id() checking for an explicit return value
       self.assertTrue(get_device_id()!=None)
+	  
   def test_generate_cert(self):
   #test def generate_cert() whether the return result is an array
   	gcval=None; bootstrapping=is_bootstrapping()
@@ -52,14 +67,17 @@ class unittests(unittest.TestCase):
 	if not isinstance(gcval, list):gcval=None
 	  
       self.assertTrue(gcval!=None)
+	  
   def test_get_ca_cert(self):
   #test def get_ca_cert() checking for an explicit return value
       self.assertTrue(get_ca_cert()!=None)
+	  
   def test_get_open_ports(self):
   #test def get_open_ports() checking for an explicit return value
       self.assertTrue(get_open_ports()!=None)
+	  
   def test_send_ping(self):
-  #test def send_ping() checking for an explicit return value
+  #test def send_ping() check for correct function
       capturedOutput = StringIO.StringIO()
       sys.stdout = capturedOutput
       send_ping()
@@ -67,5 +85,6 @@ class unittests(unittest.TestCase):
 	  printtest=capturedOutput.getvalue()
       self.assertTrue(printtest!='Ping failed.')
 	  
+	  
 if __name__ == '__main__':
-    unittest.main(exit=False)
+    unittest.main(failfast=True, exit=False)
